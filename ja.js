f1c02e192176f004ce0751cca755dbdf820d7ccf
Vikash yadav <119475709+vikashyadav2552@users.noreply.github.com>
2024-06-07 10:28:25 +0530

    my first portfolio
    make my portfolio by using HTML, CSS & Js.

---
+++ ja.js
@@ -0,0 +1,18 @@
+// alert("this is my portfolio");
+var typeData=new Typed(".role",
+{
+    strings:[
+        "Full Stack Developer",
+        "Web Developer",
+        "UI-UX Developer",
+        "Backend Developer",
+        "Coder",
+    ],
+
+    loop:true,
+    typeSpeed:100,
+    backSpeed:80,
+    backDelay:1000,
+});
+
+
